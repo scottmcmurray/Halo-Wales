8223856a25808cc858f48dec67a15362f5814518
Scott <scottmcmuray@me.com>
2012-07-12 17:55:39 +0100

    Initial

---
+++ Halo:Wales/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Halo:Wales
+//
+//  Created by Scott McMurray on 12/07/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Halo:Wales/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Halo:Wales
+//
+//  Created by Scott McMurray on 12/07/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
