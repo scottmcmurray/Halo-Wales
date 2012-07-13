5a3a3f41358209face6e42c3e547a0b8e96f6646
Scott <scottmcmuray@me.com>
2012-07-13 02:33:02 +0100

    Bubble

--- Halo:Wales/BubbleShield.m
+++ Halo:Wales/BubbleShield.m
@@ -7,12 +7,40 @@
 //
 
 #import "BubbleShield.h"
+#import "HelloWorldLayer.h"
 
 @implementation BubbleShield
+
+-(void) setUp
+{    
+    spriteFrameFile = @"halo.plist";
+    spriteBatchNodeFile = @"halo.png";
+    spriteInitialFrameName = @"066.png";
+    [self registerSprite];
+    
+    self->animations = [NSMutableDictionary dictionaryWithCapacity:1];
+    
+    CCAnimation *startBubble = [CCScaleTo actionWithDuration:0.0f scale:0.01f];
+	CCAnimation *expandBubbleAnimation = [CCScaleTo actionWithDuration:0.8f scale:1.0f];
+	
+	[self->animations setObject:startBubble forKey:@"StartBubble"];
+    [self->animations setObject:expandBubbleAnimation forKey:@"ExpandBubble"];
+
+    [self registerAnimations];
+}
 
 +(void) deployBubble
 {
 	[[SimpleAudioEngine sharedEngine] playEffect:@"bubble-shield.mp3"];
+	
+	CGSize size = [[CCDirector sharedDirector] winSize];
+	
+	BubbleShield *bubbleShield = [BubbleShield node];
+	[bubbleShield setUp];
+	bubbleShield.sprite.position = CGPointMake(0.0f, 0.0f);
+	
+	[[HelloWorldLayer scene] addChild:bubbleShield.sprite];
+//	[bubbleShield run];
 }
 
 @end
